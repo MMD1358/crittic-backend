76f9634ccd48f410ae118c8e0b129e79c0452099
MMD1358 <xdeadx1358@gmail.com>
2026-02-13 18:56:39 +0100

    13-02-2026 (Commit inicial)

---
+++ src/main/java/daw2/mariomontes/crittic/CritticApplication.java
@@ -0,0 +1,13 @@
+package daw2.mariomontes.crittic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw2/mariomontes/crittic/CritticApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2.mariomontes.crittic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
